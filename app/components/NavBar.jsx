3265a2856f0c3e808deaed195d04d7c615a46391
thomastran2k2 <thomastran1911@gmail.com>
2025-08-31 03:02:08 +0930

    unoptimize image

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="/GovAsk logo darkbg.png" className="h-20" alt="GovAsk Logo" />
+        <Image src="/GovAsk logo darkbg.png" className="h-20" alt="GovAsk Logo" />
 
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
